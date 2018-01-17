f54b22bce1909da4c94a8c1eb50d01a3c6ddcf64
gowthamkumanduri <k.gowthamraghava@gmail.com>
2018-01-17 18:39:25 +0530

    elevencommit

--- experiments/jan16tasks/datadynamiccmd1.py
+++ experiments/jan16tasks/datadynamiccmd1.py
@@ -14,29 +14,33 @@ if not  os.path.isfile(file_argument2):
 	print("Sorry , file doesnt exists")
 	exit(12)
 
-entries = {}
-
 file_one_cursor = open(file_argument1)
 file_two_cursor = open(file_argument2)
 
 file_read1 = csv.DictReader(file_one_cursor)
 file_read2 = csv.DictReader(file_two_cursor)
 
+count = 0
 def conditional_check(condition):
 	column = condition.split("=")[0]
 	value  = condition.split("=")[1]
 	#print column,value
+	end_dict = {}
 	for line in file_read1:
-		entries[line['id']]=line
-		if column in entries[line['id']]:
-			if value in column:
-				for line1 in file_read2:
-					if(line1['id']==entries[line['id']]):
-						print line1['id']
-						entries[line['id']].update(line1)
-						print entries[line['id']]
-
-
+		if column in line:
+			if line[column]== value:
+				print line
+				c+=1
+				if (c<=1):
+					end_dict[line['id']]= line
+					print end_dict[line['id']]
+		
+	for line1 in file_read2:
+		print line1
+		if line1['id']==end_dict[line['id']]:
+			end_dict.update(line1)
+			print end_dict
+					
 conditional_check(condition)
 
 
