cb0f5f42bb174dfd45479fb4a13f65365e21acbe
gowthamkumanduri <k.gowthamraghava@gmail.com>
2018-01-19 17:19:37 +0530

    fifteenth commit

--- experiments/jan18tasks/retrieverangedata.py
+++ experiments/jan18tasks/retrieverangedata.py
@@ -4,29 +4,35 @@ import sys
 
 arg =sys.argv[1]
 
-row1=arg.split(",")[0]
-col1=arg.split(",")[1]
+rowx=arg.split(",")[0]
+colx=arg.split(",")[1]
 
-row2=int(sys.argv[2])
-col2=int(sys.argv[3])
+rowspawn=int(sys.argv[2])
+colspawn=int(sys.argv[3])
 
-row2+=1
-col2+=1
+rowspawn+=1
+colspawn+=1
 s=" "
 
 file_path="/home/gowtham/Desktop/experiments/jan16tasks/data1 .xlsx"
 
 book = xlrd.open_workbook(file_path,"rb")
+sheet= book.sheet_by_index(0)
 
-
-sheets=book.sheet_by_index(0)
-
-
-for row in range(int(row1),int(row2)):
-	row_values=sheets.row_values(row)
-	for cols in range(int(col1),int(col2)):
-		s=s+row_values[cols]+"	"
-
+for row in range(int(rowx),int(rowspawn)):
+	row_values=sheet.row_values(row)
+	for cols in range(int(colx),int(colspawn)):
+		s=s+str(row_values[cols])+"	"
 	s=s+"\n"
-	
-print s
+#print (s)	
+d={}
+dict_list = []
+keys = [sheet.cell(0, col_index).value for col_index in range(sheet.ncols)]
+print keys
+for row_index in range(int(rowx),int(rowspawn)):
+	for col_index in range(int(colx),int(colspawn)):
+		d[keys[col_index]] = sheet.cell(row_index, col_index).value
+	dict_list.append(d)
+
+print dict_list
+
